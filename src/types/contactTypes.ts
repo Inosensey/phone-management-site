09244452bce3418e19999e5779bcfd333eb21052
Inosensey <dingcong.bae@gmail.com>
2025-03-29 15:38:51 +0800

    Created 	ypes/ folder with contactTypes.ts and userTypes.ts.

---
+++ src/types/contactTypes.ts
@@ -0,0 +1,10 @@
+export type contactInfoTypes = {
+    user_id: number;
+    first_name: string;
+    last_name: string;
+    email_address: string;
+    contact_number: string;
+    contact_profile_photo: string;
+    _id: string;
+    __v: number;
+}
---
+++ src/types/userTypes.ts
@@ -0,0 +1,11 @@
+export type userInfoType = {
+    id: number;
+    username: string;
+    email: string;
+    first_name: string;
+    last_name:  string;
+    role_id: number;
+    role_name: string;
+    status_id?: number;
+    status_name?: string;
+};
